5dc3c3eb4090fa4faf179c102b658bae256280ac
David McClure <david.mcclure@virginia.edu>
2013-09-13 15:50:36 -0700

    Gets rid of Geoserver class.

--- lib/geoloader/geotiff.rb
+++ lib/geoloader/geotiff.rb
@@ -6,20 +6,15 @@ require 'fileutils'
 module Geoloader
   class Geotiff
 
-    # Copy the input file for processing.
-    # @param [String] path
-    # @param [String] suffix
     def initialize path, suffix = "processed"
       @path = "#{File.dirname(path)}/#{File.basename(path, ".tif")}.#{suffix}.tif"
       FileUtils.cp path, @path
     end
 
-    # Remove black borders created by ArcGIS.
     def remove_border
       system "gdalwarp -srcnodata 0 -dstalpha #{@path} #{@path}"
     end
 
-    # Add a well-formed EPSG:4326 header.
     def build_header
       system "gdal_translate -of GTiff -a_srs EPSG:4326 #{@path} #{@path}_"
       FileUtils.rm @path
