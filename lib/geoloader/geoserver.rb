b2646075977c5f210b63712d68ebe23ef21ed726
David McClure <david.mcclure@virginia.edu>
2013-09-13 15:10:49 -0700

    Starts to sketch in Geoserver class.

--- lib/geoloader/geoserver.rb
+++ lib/geoloader/geoserver.rb
@@ -4,7 +4,19 @@
 module Geoloader
   class Geoserver
 
+    # @param [OrderedHash] options
+    # @option options [String] :url
+    # @option options [String] :username
+    # @option options [String] :password
+    def initialize options = {}
+      @config = options
+    end
+
     def upload_geotiff
+      # TODO
+    end
+
+    def upload_shapefile
       # TODO
     end
 
--- lib/geoloader/geotiff.rb
+++ lib/geoloader/geotiff.rb
@@ -6,16 +6,21 @@ require 'fileutils'
 module Geoloader
   class Geotiff
 
+    # Copy the input file for processing.
+    # @param [String] path
+    # @param [String] suffix
     def initialize path, suffix = "processed"
       @path = "#{File.dirname(path)}/#{File.basename(path, ".tif")}.#{suffix}.tif"
       FileUtils.cp path, @path
     end
 
+    # Remove black borders created by ArcGIS.
     def remove_border
       system "gdalwarp -srcnodata 0 -dstalpha #{@path} #{@path}"
     end
 
-    def rebuild_header
+    # Add a well-formed EPSG:4326 header.
+    def build_header
       system "gdal_translate -of GTiff -a_srs EPSG:4326 #{@path} #{@path}_"
       FileUtils.rm @path
       FileUtils.mv "#{@path}_", @path
@@ -23,7 +28,7 @@ module Geoloader
 
     def process
       remove_border
-      rebuild_header
+      build_header
     end
 
   end
