03c804a0ed49ea03d2c26eaf4bb95cc3755a428b
David McClure <david.mcclure@virginia.edu>
2013-09-23 12:15:08 -0700

    Updates Geonetwork and Geoserver classes to pull from global config hash.

--- lib/geoloader/geonetwork.rb
+++ lib/geoloader/geonetwork.rb
@@ -7,18 +7,12 @@ require 'builder'
 module Geoloader
   class Geonetwork
 
-    # Set connection parameters.
-    #
-    # @param [OrderedHash] config
-    # @param [String] config :url
-    # @param [String] config :username
-    # @param [String] config :password
-    # @param [Int] config :group
-    def initialize config = {}
-      @config = config
-      @resource = RestClient::Resource.new @config[:url], {
-        :user     => @config[:username],
-        :password => @config[:password],
+    # Create Geonetwork resource.
+    def initialize
+      @config = Geoloader.config.geonetwork
+      @resource = RestClient::Resource.new @config.url, {
+        :user     => @config.username,
+        :password => @config.password,
         :headers  => { :content_type => :xml }
       }
     end
@@ -46,7 +40,7 @@ module Geoloader
     # @return [RestClient::Response]
     def metadata_insert path, style_sheet = "_none_", category = "_none_"
       post "metadata.insert", self.class.xml.request { |r|
-        r.group @config[:group]
+        r.group @config.group
         r.data { |d| d.cdata! File.read(path) }
         r.category category
         r.styleSheet style_sheet
--- lib/geoloader/geoserver.rb
+++ lib/geoloader/geoserver.rb
@@ -6,27 +6,21 @@ require 'rest_client'
 module Geoloader
   class Geoserver
 
-    # Set connection parameters.
-    #
-    # @param [OrderedHash] config
-    # @param [String] config :url
-    # @param [String] config :username
-    # @param [String] config :password
-    # @param [String] config :workspace
-    def initialize config = {}
-      @config = config
-      @resource = RestClient::Resource.new @config[:url], {
-        :user     => @config[:username],
-        :password => @config[:password]
+    # Create Geoserver resource.
+    def initialize
+      @config = Geoloader.config.geoserver
+      @resource = RestClient::Resource.new @config.url, {
+        :user     => @config.username,
+        :password => @config.password
       }
     end
 
-    # Create a new coverage store and publish a layer from a GeoTIFF.
+    # Create a new coverage store from a GeoTIFF.
     #
     # @param [Geoloader::Geotiff] tiff
     # @return [RestClient::Response]
     def upload_geotiff tiff
-      service = "workspaces/#{@config[:workspace]}/coveragestores/#{tiff.base}/file.geotiff"
+      service = "workspaces/#{@config.workspace}/coveragestores/#{tiff.base}/file.geotiff"
       @resource[service].put File.read(tiff.path)
     end
 
