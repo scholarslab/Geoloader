ce160ae3141d4b5a2e81752b4fe068d1970fe84e
David McClure <david.mcclure@virginia.edu>
2013-09-23 11:48:54 -0700

    Sketching in opt-in YAML configuration.

--- lib/geoloader.rb
+++ lib/geoloader.rb
@@ -1,6 +1,7 @@
 
 # vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
 
+require 'geoloader/config'
 require 'geoloader/geotiff'
 require 'geoloader/geonetwork'
 require 'geoloader/geoserver'
--- lib/geoloader/config.rb
+++ lib/geoloader/config.rb
@@ -1,17 +1,28 @@
 
 # vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
 
+require 'confstruct'
+require 'yaml'
+
 module Geoloader
 
   @config = Confstruct::Configuration.new do
     geoserver do
-      url        "http://admin:geoserver@localhost:8080/geoserver/rest"
-      username   "admin"
-      password   "geoserver"
-      workspace  "geoloader"
+      url       "http://admin:geoserver@localhost:8080/geoserver/rest"
+      username  "admin"
+      password  "geoserver"
+      workspace "geoloader"
     end
   end
 
+  # Set configuration options from YAML.
+  #
+  # @param [String] path
+  def self.configure path
+    @config.configure YAML::load(File.read(path))
+  end
+
+  # Get the configuration object.
   def self.config
     @config
   end
