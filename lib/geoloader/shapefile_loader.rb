2a1952e693bc515526bc52dfa727935e1aa9ce4f
David McClure <david.mcclure@virginia.edu>
2013-09-25 11:03:26 -0700

    Sketches ShapefileLoader class.

--- lib/geoloader/shapefile_loader.rb
+++ lib/geoloader/shapefile_loader.rb
@@ -2,13 +2,14 @@
 # vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
 
 module Geoloader
-  class GeotiffLoader
+  class ShapefileLoader
 
     # Create file and service instances.
     #
     # @param [String] file_name
     def initialize file_name
-      @geotiff    = Geoloader::Geotiff.new file_name
+      @shapefile  = Geoloader::Shapefile.new file_name
+      @postgis    = Geoloader::Postgis.new
       @geoserver  = Geoloader::Geoserver.new
       @geonetwork = Geoloader::Geonetwork.new
     end
@@ -16,8 +17,9 @@ module Geoloader
     # Process and upload the geotiff.
     def work
       begin
-        @geoserver.upload_geotiff @geotiff
-        @geonetwork.metadata_insert @geotiff
+        @postgis.add_table @shapefile
+        @geoserver.publish_table @shapefile
+        @geonetwork.metadata_insert @shapefile
       rescue
         # Failure.
       else
