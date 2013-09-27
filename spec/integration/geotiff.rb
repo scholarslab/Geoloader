e92fbb1352cdcc61e890dc5106a621148fdefa53
David McClure <david.mcclure@virginia.edu>
2013-09-26 18:51:09 -0700

    Adds basic testing rig.

---
+++ spec/integration/geotiff.rb
@@ -0,0 +1,13 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require 'geoloader'
+require 'minitest/autorun'
+
+class TestGeotiff < Minitest::Test
+
+  def test_test
+    assert_equal true, true
+  end
+
+end
