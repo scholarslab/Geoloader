619d3fc0cf19a04660a2a6f2efdab851875bbeb6
David McClure <david.mcclure@virginia.edu>
2013-09-27 08:23:28 -0700

    Adds shells for loader class integration tests.

---
+++ spec/integration/geotiff_loader.rb
@@ -0,0 +1,17 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require 'geoloader'
+require 'minitest/autorun'
+
+describe Geoloader::GeotiffLoader do
+
+  it "should create a new coveragestore on Geoserver" do
+    # TODO
+  end
+
+  it "should publish a new layer on Geoserver" do
+    # TODO
+  end
+
+end
---
+++ spec/integration/shapefile_loader.rb
@@ -0,0 +1,21 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require 'geoloader'
+require 'minitest/autorun'
+
+describe Geoloader::ShapefileLoader do
+
+  it "should create a new PostGIS database" do
+    # TODO
+  end
+
+  it "should create a new datastore on Geoserver" do
+    # TODO
+  end
+
+  it "should publish a new layer on Geoserver " do
+    # TODO
+  end
+
+end
