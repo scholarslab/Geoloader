48ecf7d1efcbe4ce3f54f19bc3e8ec9d948c0c74
David McClure <david.mcclure@virginia.edu>
2013-09-26 17:13:59 -0700

    Moves example files into /fixtures.

--- examples/geotiff.rb
+++
@@ -1,8 +0,0 @@
-
-# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
-
-require 'geoloader'
-
-Geoloader.configure_from_yaml "../config.yaml"
-loader = Geoloader::GeotiffLoader.new ARGV[0]
-loader.work
--- examples/shapefile.rb
+++
@@ -1,8 +0,0 @@
-
-# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
-
-require 'geoloader'
-
-Geoloader.configure_from_yaml "../config.yaml"
-loader = Geoloader::ShapefileLoader.new ARGV[0]
-loader.work
