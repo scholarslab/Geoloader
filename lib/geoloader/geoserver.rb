7ad858e1c65e4568cc8ffdccb6308d6e9bc7b971
David McClure <david.mcclure@virginia.edu>
2013-10-01 11:28:08 -0700

    Adds config-controlled prefix to postgres databases.

--- lib/geoloader/geoserver.rb
+++ lib/geoloader/geoserver.rb
@@ -57,7 +57,7 @@ module Geoloader
           c.port      Geoloader.config.postgis.port
           c.user      Geoloader.config.postgis.username
           c.passwd    Geoloader.config.postgis.password
-          c.database  shapefile.base_name
+          c.database  shapefile.db_name
           c.dbtype    "postgis"
         }
       }
--- lib/geoloader/shapefile.rb
+++ lib/geoloader/shapefile.rb
@@ -7,7 +7,15 @@ require 'pg'
 module Geoloader
   class Shapefile < Asset
 
-    attr_reader :sql_path
+    attr_reader :db_name, :sql_path
+
+    # Set the database name.
+    #
+    # @param [String] file_path
+    def initialize(file_path)
+      super(file_path)
+      @db_name = Geoloader.config.postgis.prefix + @base_name
+    end
 
     # Convert the file to SQL for PostGIS.
     def generate_sql
@@ -17,19 +25,19 @@ module Geoloader
 
     # Create a PostGIS-enabled database and connect to it.
     def create_database
-      system "createdb #{self.class.psql_options} #{@base_name}"
-      system "psql #{self.class.psql_options} -d #{@base_name} -c 'CREATE EXTENSION postgis;'"
+      system "createdb #{self.class.psql_options} #{@db_name}"
+      system "psql #{self.class.psql_options} -d #{@db_name} -c 'CREATE EXTENSION postgis;'"
       connect
     end
 
     # Drop the PostGIS database.
     def drop_database
-      system "dropdb #{self.class.psql_options} #{@base_name}"
+      system "dropdb #{self.class.psql_options} #{@db_name}"
     end
 
     # Source shapefile SQL to the new database.
     def source_sql
-      system "psql #{self.class.psql_options} -d #{@base_name} -f #{@sql_path}"
+      system "psql #{self.class.psql_options} -d #{@db_name} -f #{@sql_path}"
     end
 
     # Fetch a list of layers in the database.
@@ -58,7 +66,7 @@ module Geoloader
         :host => Geoloader.config.postgis.host,
         :port => Geoloader.config.postgis.port,
         :user => Geoloader.config.postgis.username,
-        :dbname => @base_name
+        :dbname => @db_name
       )
     end
 
