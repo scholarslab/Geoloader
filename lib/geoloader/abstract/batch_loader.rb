e2a83c042d068ecef7936c8f0907c63a77cb8cfa
David McClure <david.mcclure@virginia.edu>
2013-10-17 09:17:19 -0700

    Adds BatchLoader class.

---
+++ lib/geoloader/abstract/batch_loader.rb
@@ -0,0 +1,36 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+module Geoloader
+  class BatchLoader
+
+    #
+    # Read batch manifest and metadata.
+    #
+    # @param [String] file_name
+    #
+    def initialize(file_path)
+      @file_path = File.expand_path(file_path)
+      @yaml = Yaml::load(File.read(file_path))
+    end
+
+    #
+    # Expand the file manifest and load each asset.
+    #
+    def load
+
+      Dir.glob("#{File.dirname(@file_path)}/#{@yaml["files"]}") do |file|
+        case File.extname(file)
+        when ".tif"
+          loader = Geoloader::GeotiffLoader.new(file, @yaml)
+          loader.load
+        when ".shp"
+          loader = Geoloader::ShapefileLoader.new(file, @yaml)
+          loader.load
+        end
+      end
+
+    end
+
+  end
+end
