16cfa20e272b93d006ab984d8791cc02808272ba
David McClure <david.mcclure@virginia.edu>
2013-10-22 15:03:10 -0700

    Adds separate Routines module.

---
+++ lib/geoloader/routines.rb
@@ -0,0 +1,89 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require "terminal-table"
+require "resque"
+
+module Geoloader
+  module Routines
+
+    #
+    # Load a batch manifest.
+    #
+    # @param [String] manifest
+    # @param [Boolean] resque
+    #
+    def load(file_path, resque)
+
+      # Read the manifest.
+      manifest_path = File.expand_path(file_path)
+      manifest = YAML::load(File.read(manifest_path))
+
+      # For each matched file in the manifest:
+      Dir.glob("#{File.dirname(manifest_path)}/#{manifest["files"]}") do |f|
+
+        # Get a loader class.
+        loader = case File.extname(f)
+        when ".shp" then
+          Geoloader::ShapefileLoader
+        when ".tif"
+          Geoloader::GeotiffLoader
+        else
+          next
+        end
+
+        # Execute or enqueue the load.
+        if resque
+          Resque.enqueue(loader, f, manifest)
+        else
+          loader.new(f, manifest).load
+        end
+
+      end
+
+    end
+
+    #
+    # Clear all assets in a workspace.
+    #
+    # @param [String] workspace
+    #
+    def clear(workspace)
+
+      # Delete Geoserver stores.
+      Geoloader::Geoserver.new.delete_workspace(workspace)
+
+      # Drop PostGIS tables.
+      Geoloader::Postgres.new.drop_databases_by_workspace(workspace)
+
+      # Delete Solr documents.
+      Geoloader::Solr.new.delete_by_workspace(workspace)
+
+    end
+
+    #
+    # List all workspaces with asset counts.
+    #
+    def list
+
+      # Query for workspace counts. 
+      counts = Geoloader::Solr.new.get_workspace_counts
+
+      # Pretty-print the table.
+      Terminal::Table.new(
+        :title    => "GEOLOADER",
+        :headings => ["Workspace", "# Assets"],
+        :rows     => counts
+      )
+
+    end
+
+    #
+    # Spin up a Resque worker.
+    #
+    def work
+      Resque::Worker.new("geoloader").work
+    end
+
+  end
+end
