bbedb1d910953ed1e041c124f96be748e21db371
David McClure <david.mcclure@virginia.edu>
2013-10-24 13:53:19 -0700

    Moves CLI class into core module.

---
+++ lib/geoloader/cli.rb
@@ -0,0 +1,32 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require "terminal-table"
+require "thor"
+
+module Geoloader
+  class CLI < Thor
+
+    desc "load MANIFEST", "Load a YAML batch manifest"
+    option "resque", :aliases => "q", :type => :boolean, :default => false
+    def load(manifest)
+      Geoloader::Routines.load(manifest, options[:resque])
+    end
+
+    desc "clear WORKSPACE", "Clear a workspace"
+    def clear(workspace)
+      Geoloader::Routines.clear(workspace)
+    end
+
+    desc "list", "List workspaces and asset counts"
+    def list
+      puts Geoloader::Routines.list
+    end
+
+    desc "work", "Start a Resque worker"
+    def work
+      Geoloader::Routines.work
+    end
+
+  end
+end
