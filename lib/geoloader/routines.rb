dc081ed60aff5044c3ddaf9fc8df115aaeb3eb70
David McClure <david.mcclure@virginia.edu>
2013-10-31 14:14:09 -0700

    Stubs out a Routines module.

---
+++ lib/geoloader/routines.rb
@@ -0,0 +1,35 @@
+
+# vim: set tabstop=2 shiftwidth=2 softtabstop=2 cc=100;
+
+require "terminal-table"
+require "thor"
+
+module Geoloader
+  module Routines
+
+    def load_asset
+      # TODO
+    end
+
+    def load_geotiff
+      # TODO
+    end
+
+    def load_shapefile
+      # TODO
+    end
+
+    def count_assets
+      # TODO
+    end
+
+    def clear_workspace
+      # TODO
+    end
+
+    def start_worker
+      # TODO
+    end
+
+  end
+end
