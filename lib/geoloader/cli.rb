e14cc6852bac4677351edf76d0061fcd803596f1
David McClure <david.mcclure@virginia.edu>
2014-02-10 18:25:30 -0800

    Adds solr/geoserver/geonetwork-specific subcommands to the CLI app.

--- lib/geoloader/cli.rb
+++ lib/geoloader/cli.rb
@@ -7,7 +7,124 @@ require "thor"
 module Geoloader
   module CLI
 
-    class Geoloader < Thor
+    class Solr < Thor
+
+      include Tasks
+
+      desc "load [FILES]", "Load files to Solr"
+      option :workspace,  :aliases => "-w", :type => :string
+      option :queue,      :aliases => "-q", :type => :boolean, :default => false
+      option :metadata,   :aliases => "-m", :type => :string
+      def load(*files)
+
+        # If no workspace is defined, use the global default.
+        workspace = (options[:workspace] or Geoloader.config.workspaces.production)
+
+        # If provided, load the metadata YAML manifest.
+        if options[:metadata]
+          metadata = YAML::load(File.read(File.expand_path(options[:metadata])))
+        else
+          metadata = {}
+        end
+
+        # Load (or enqueue) the files.
+        files.each { |file_path|
+          case File.extname(file_path)
+          when ".tif" # GEOTIFF
+            load_geotiff_solr(file_path, workspace, metadata, options[:queue])
+          when ".shp" # SHAPEFILE
+            load_shapefile_solr(file_path, workspace, metadata, options[:queue])
+          end
+        }
+
+      end
+
+      desc "clear [WORKSPACE]", "Clear all documents in a workspace"
+      def clear(workspace)
+        clear_solr_workspace(workspace) rescue nil
+      end
+
+    end
+
+    class Geoserver < Thor
+
+      include Tasks
+
+      desc "load [FILES]", "Load files to Geoserver"
+      option :workspace,  :aliases => "-w", :type => :string
+      option :queue,      :aliases => "-q", :type => :boolean, :default => false
+      option :metadata,   :aliases => "-m", :type => :string
+      def load(*files)
+
+        # If no workspace is defined, use the global default.
+        workspace = (options[:workspace] or Geoloader.config.workspaces.production)
+
+        # If provided, load the metadata YAML manifest.
+        if options[:metadata]
+          metadata = YAML::load(File.read(File.expand_path(options[:metadata])))
+        else
+          metadata = {}
+        end
+
+        # Load (or enqueue) the files.
+        files.each { |file_path|
+          case File.extname(file_path)
+          when ".tif" # GEOTIFF
+            load_geotiff_geoserver(file_path, workspace, metadata, options[:queue])
+          when ".shp" # SHAPEFILE
+            load_shapefile_geoserver(file_path, workspace, metadata, options[:queue])
+          end
+        }
+
+      end
+
+      desc "clear [WORKSPACE]", "Clear a Geoserver workspace"
+      def clear(workspace)
+        clear_geoserver_workspace(workspace) rescue nil
+      end
+
+    end
+
+    class Geonetwork < Thor
+
+      include Tasks
+
+      desc "load [FILES]", "Load files to Geonetwork"
+      option :workspace,  :aliases => "-w", :type => :string
+      option :queue,      :aliases => "-q", :type => :boolean, :default => false
+      option :metadata,   :aliases => "-m", :type => :string
+      def load(*files)
+
+        # If no workspace is defined, use the global default.
+        workspace = (options[:workspace] or Geoloader.config.workspaces.production)
+
+        # If provided, load the metadata YAML manifest.
+        if options[:metadata]
+          metadata = YAML::load(File.read(File.expand_path(options[:metadata])))
+        else
+          metadata = {}
+        end
+
+        # Load (or enqueue) the files.
+        files.each { |file_path|
+          case File.extname(file_path)
+          when ".tif" # GEOTIFF
+            load_geotiff_geonetwork(file_path, workspace, metadata, options[:queue])
+          when ".shp" # SHAPEFILE
+            load_shapefile_geonetwork(file_path, workspace, metadata, options[:queue])
+          end
+        }
+
+      end
+
+      desc "clear [WORKSPACE]", "Clear a Geonetwork group"
+      def clear(workspace)
+        clear_geonetwork_group(workspace) rescue nil
+      end
+
+    end
+
+    class App < Thor
 
       include Tasks
 
@@ -32,36 +149,16 @@ module Geoloader
 
         files.each { |file_path|
           case File.extname(file_path)
-
           when ".tif" # GEOTIFF
-
             options[:services].each { |service|
               send("load_geotiff_#{service}", file_path, workspace, metadata, options[:queue])
             }
-
           when ".shp" # SHAPEFILE
-
             options[:services].each { |service|
               send("load_shapefile_#{service}", file_path, workspace, metadata, options[:queue])
             }
-
           end
         }
-
-      end
-
-      desc "list", "List workspaces and asset counts"
-      def list
-
-        # Query for workspace counts.
-        counts = Geoloader::Solr.new.get_workspace_counts
-
-        # Render the table.
-        puts Terminal::Table.new(
-          :title    => "GEOLOADER",
-          :headings => ["Workspace", "# Assets"],
-          :rows     => counts
-        )
 
       end
 
@@ -77,41 +174,14 @@ module Geoloader
         Resque::Worker.new("geoloader").work
       end
 
-      desc "solr [SUBCOMMAND]", "Manage Solr documents."
+      desc "solr [SUBCOMMAND]", "Manage Solr documents"
       subcommand "solr", Solr
 
       desc "geoserver [SUBCOMMAND]", "Manage Geoserver stores and layers"
       subcommand "geoserver", Geoserver
 
-      desc "geonetwork [SUBCOMMAND]", "Manage Geonetwork records"
+      desc "geoserver [SUBCOMMAND]", "Manage Geonetwork records"
       subcommand "geonetwork", Geonetwork
-
-    end
-
-    class Solr < Thor
-
-      desc "load [FILES]", "Load files to Solr"
-      def load
-        puts "solr load"
-      end
-
-    end
-
-    class Geoserver < Thor
-
-      desc "load [FILES]", "Load files to Geoserver"
-      def load
-        puts "geoserver load"
-      end
-
-    end
-
-    class Geonetwork < Thor
-
-      desc "load [FILES]", "Load files to Geonetwork"
-      def load
-        puts "geonetwork load"
-      end
 
     end
 
